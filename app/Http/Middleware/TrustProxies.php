a66a047c0f60ddc37736f2884be6b0b6d2584f75
fadlyatthoriq <fadlyatthoriq15@gmail.com>
2025-06-25 13:12:10 +0700

    Adding some feature and fixing minor bugs

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,5 +19,9 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = 
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO;
 }
